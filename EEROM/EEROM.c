122acc7ace4a6fe28077a677494201e9e675eac7
houaohui <59907145+houaohui@users.noreply.github.com>
2020-02-28 20:32:58 +0800

    Update EEROM.c

--- EEROM/EEROM.c
+++ EEROM/EEROM.c
@@ -13,13 +13,15 @@ void delay_5us()
 }
 void delay(uchar i)
 {
-	uchar a=60000;
-	for(i=60000;i>1;i--)
+	uchar a=120;
+	for(i=120;i>1;i--)
 	{
 		while(a--);
 	}
 }
-
+/*-----------------
+初始化总线
+---------------*/
 void I2C_init()
 {
 	SDA=1;
@@ -27,8 +29,10 @@ void I2C_init()
 	SCL=1;
 	while(0);
 }
-
-void I2C_START() //ÆðÊ¼ÐÅºÅ
+/*------------------
+起始信号
+---------------*/
+void I2C_START()
 {
 	SCL=1;
 	while(0);
@@ -37,7 +41,9 @@ void I2C_START() //ÆðÊ¼ÐÅºÅ
 	SDA=0;
 	delay_5us();
 }
-
+/*------------------
+停止信号
+--------------*/
 void I2C_STOP()
 {
 	SDA=0;
@@ -47,8 +53,10 @@ void I2C_STOP()
 	SDA=1;
 	delay_5us();
 }
-
-void master_ACK(bit i) //Ö÷»ú·¢ËÍÓ¦´ð
+/*-----------------
+主机发送应答
+-----------------*/
+void master_ACK(bit i)
 {
 	SCL=1;
 	while(0);
@@ -68,7 +76,9 @@ void master_ACK(bit i) //Ö÷»ú·¢ËÍÓ¦´ð
 	SDA=1;
 	while(0);
 }
-
+/*---------------
+检测应答
+----------------*/
 bit TEST_ACK()
 {
 	SCL=1;
@@ -87,8 +97,9 @@ bit TEST_ACK()
 		return(1);
 	}
 }
-/**/
-
+/*--------------
+发送一个字节
+----------------*/
 void I2C_send_byte(uchar byte)
 {
 	uchar i;
@@ -115,7 +126,9 @@ void I2C_send_byte(uchar byte)
 	SDA=1;
 	while(0);
 }
-
+/*----------------
+接收一个字节
+-----------------*/
 uchar I2C_read_byte()
 {
 	uchar dat,i;
@@ -129,11 +142,11 @@ uchar I2C_read_byte()
 		while(0);
 		if(SDA)
 		{
-			dat=dat|0x01; //
+			dat=dat|0x01;
 		}
 		else
 		{
-			dat=dat&0xfe; //
+			dat=dat&0xfe;
 		}
 		while(0);
 		SCL=0;
@@ -147,29 +160,33 @@ uchar I2C_read_byte()
 }
 
 
-
+/*-------------------
+在EEPROM中的一个地址写入一个字节的数据
+--------------------*/
 bit I2C_transmitdata(uchar ADDR,DAT)
 {
 	I2C_START();
 	I2C_send_byte(AT24C02_ADDR+0);
 	if(!TEST_ACK())
 	{
-		//return(0);
+		return(0);           //如果没有应答，直接退出
 	}
 	I2C_send_byte(ADDR);
 	if(!TEST_ACK())
 	{
-		//return(0);
+		return(0);
 	}
 	I2C_send_byte(DAT);
 	if(!TEST_ACK())
 	{
-		//return(0);
+		return(0);
 	}
 	I2C_STOP();
 	return(1);
 }
-
+/*-----------------------
+从EEPROM中读取一个字节的数据
+------------------------*/
 uchar I2C_receivedata(uchar ADDR)
 {
 	uchar DAT;
@@ -177,19 +194,19 @@ uchar I2C_receivedata(uchar ADDR)
 	I2C_send_byte(AT24C02_ADDR+0);
 	if(!TEST_ACK())
 	{
-		//return(0);
+		return(0);
 	}
 	I2C_send_byte(ADDR);
 	if(!TEST_ACK())
 	{
-		//return(0);
+		return(0);
 	}
 	
 	I2C_START();
 	I2C_send_byte(AT24C02_ADDR+1);
 	if(!TEST_ACK())
 	{
-		//return(0);
+		return(0);
 	}
 	DAT=I2C_read_byte();
 	master_ACK(0);
@@ -203,78 +220,28 @@ uchar I2C_receivedata(uchar ADDR)
 void main()
 {
 	uchar ACK_flag=0,a=0,i,m=0;
-	delay_5us();
-	delay_5us();
-	delay_5us();
-	P11=0;
+	P11=0;                    //使能P0
 	P20=0;
 	P21=1;
-	P21=1;
+	P21=1;                    //选择led流水灯
 	I2C_init();
-//	I2C_START();
-//	I2C_send_byte(AT24C02_ADDR+0);
-//	if(!TEST_ACK())
-//	{
-//		ACK_flag=1;
-//	}
-//	I2C_send_byte(8);
-//	if(!TEST_ACK())
-//	{
-//		ACK_flag=1;
-//	}
-//	I2C_send_byte(0xaa);
-//	if(!TEST_ACK())
-//	{
-//		ACK_flag=1;
-//	}
-//	I2C_STOP();
-	for(i=0;i<8;i++)
+	
+	
+	for(i=0;i<8;i++)          //在EEROM中的0到8号储存空间写入0到8
 	{
-	I2C_transmitdata(i,m);
-	m++;
-	delay(60000);
+		I2C_transmitdata(i,m);
+		m++;
+		delay(60000);
 	}
 	i=7;
-	while(1)
+	while(1)                  //重复读取0到8存储空间，并通过流水灯的效果展示
 	{
-	P0=I2C_receivedata(i);
+		P0=I2C_receivedata(i);
 		i--;
-	if(i==-1)
-		i=7;
-	delay(60000);
-	delay(60000);
-	delay(60000);
-	delay(60000);
-	delay(60000);
-	delay(60000);
+		if(i==-1)i=7;
+		delay(60000);
+		delay(60000);
 	}
-	
-//	I2C_START();
-//	I2C_send_byte(AT24C02_ADDR+0);
-//	if(!TEST_ACK())
-//	{
-//		a=1;
-//	}
-//	I2C_send_byte(1);
-//	master_ACK(0);
-//	I2C_START();
-//	I2C_send_byte(AT24C02_ADDR+1);
-//	if(!TEST_ACK())
-//	{
-//		a=1;
-//	}
-//	P0=I2C_read_byte();
-//	master_ACK(0);
-//	I2C_STOP();
-//	if(ACK_flag)
-//	{
-//		P00=0;
-//	}
-//	if(a)
-//	{
-//		P07=0;
-//	}
-	
 }
 
 
