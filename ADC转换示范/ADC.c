3605ce302b3768535126e628eb5b101e7a047e61
houaohui <59907145+houaohui@users.noreply.github.com>
2020-02-28 20:09:44 +0800

    Update ADC.c

--- ADC转换示范/ADC.c
+++ ADC转换示范/ADC.c
@@ -3,14 +3,16 @@
 
 sfr PLASF = 0x9d;
 
-#define ADC_POWER 0x80//ADC开关控制位
-#define ADC_FLAG 0x10//ADC转换完成标志位
-#define ADC_START 0x08
-#define ADC_SPEEDLL 0x00
+#define ADC_POWER 0x80           //ADC开关控制位
+#define ADC_FLAG 0x10            //ADC转换完成标志位
+#define ADC_START 0x08           //ADC开始转换位
+#define ADC_SPEEDLL 0x00         //540 clocks
 unsigned char dat,a,b,c;
 unsigned char code ledchar[]=
 {0xc0,0xf9,0xa4,0xb0,0x99,0x92,0x82,0xf8,0x80,0x90};
-
+/*-------------
+延时函数
+--------------*/
 void delay(unsigned int n)
 {
 	unsigned int x;
@@ -20,26 +22,28 @@ void delay(unsigned int n)
 		while(x--);
 	}
 }
-
+/*----------------
+ADC初始化
+----------------*/
 void ADC_init()
 {
-	PLASF = 0x01;
-	ADC_RES = 0;
+	PLASF = 0x01;           //设置P10为ADC转换通道
+	ADC_RES = 0;            //清除上次转化结果
 	ADC_CONTR = ADC_POWER|ADC_SPEEDLL|ADC_START;
-	delay(2); //ËÄ¸öÊ±ÖÓÖÜÆÚ
+	delay(2);               //延时四个时钟周期
 }
 
 void main()
 {
-	P11=0;
+	P11=0;                  //使能P0
 	ADC_init();
-	IE=0xa0;
+	IE=0xa0;                //打开ADC中断
 	while(1);
 }
 
 void adc() interrupt 5 using 1
 {
-	ADC_CONTR&=!ADC_FLAG;
+	ADC_CONTR&=!ADC_FLAG;     //清除转换标志位
 	dat = ADC_RES;
 	
 	a=ledchar[dat%10];
